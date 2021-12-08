a7d0d4b99e54f0ee87e8300a6d80382d2cd32a35
Ignat <sasha-lashov@rambler.ru>
2021-12-08 21:39:42 +0300

    master

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,3 +7,28 @@ $(document).on('click', '.video__controll__img', function() {
 	$('.video__preview__img').addClass('hide').removeClass('show');
 	$('.video__controll').addClass('hide').removeClass('show');
 });
+
+$(document).on('click', '.peculiarity__vision', function() {
+	$('.peculiarity__vision .registration__peculiarity__img').addClass('regist__active');
+	$('.peculiarity__hearing .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__movement .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__vision .registration__peculiarity__text').addClass('regist__text__active');
+	$('.peculiarity__hearing .registration__peculiarity__text').removeClass('regist__text__active');
+	$('.peculiarity__movement .registration__peculiarity__text').removeClass('regist__text__active');
+});
+$(document).on('click', '.peculiarity__hearing', function() {
+	$('.peculiarity__vision .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__hearing .registration__peculiarity__img').addClass('regist__active');
+	$('.peculiarity__movement .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__vision .registration__peculiarity__text').removeClass('regist__text__active');
+	$('.peculiarity__hearing .registration__peculiarity__text').addClass('regist__text__active');
+	$('.peculiarity__movement .registration__peculiarity__text').removeClass('regist__text__active');
+});
+$(document).on('click', '.peculiarity__movement', function() {
+	$('.peculiarity__vision .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__hearing .registration__peculiarity__img').removeClass('regist__active');
+	$('.peculiarity__movement .registration__peculiarity__img').addClass('regist__active');
+	$('.peculiarity__vision .registration__peculiarity__text').removeClass('regist__text__active');
+	$('.peculiarity__hearing .registration__peculiarity__text').removeClass('regist__text__active');
+	$('.peculiarity__movement .registration__peculiarity__text').addClass('regist__text__active');
+});
